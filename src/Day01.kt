18e3b9006221270dba44538b8b4efb060242320c
Peter Hart <peter.hart@supplydynamics.com>
2021-12-01 11:55:45 -0500

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun List<String>.asInts() : List<Int> {
+        return map { a -> Integer.parseInt(a) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun List<Int>.countDecreases() : Int {
+        return drop(1).fold(Pair(0, first())) { acc, e ->
+            Pair((if (e > acc.second) acc . first +1 else acc.first), e)
+        }.first
+    }
+
+    fun part1(asInts: List<Int>): Int {
+        return asInts.countDecreases()
+    }
+
+    fun part2(input: List<Int>): Int {
+        return input.drop(2).mapIndexed {
+            i, v -> v + input[i] + input[i+1]
+        }.countDecreases()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput.asInts()) == 7)
+    check(part2(testInput.asInts()) == 5)
+
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input.asInts()))
+    println(part2(input.asInts()))
 }
