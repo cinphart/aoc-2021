4020a0f7b16cafd10290da518389138e5a1dfc02
Peter Hart <cinphart@gmail.com>
2021-12-06 12:32:59 -0500

    Day 06 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -4,17 +4,51 @@ import kotlin.math.min
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return 0
+        var counts =
+                input[0].splitToInts().groupBy { it }.entries.map { e -> e.key to e.value.size }.toMap()
+
+        (0..79).forEach {
+            counts = mapOf(
+                    0 to (counts[1]?:0),
+                    1 to (counts[2]?:0),
+                    2 to (counts[3]?:0),
+                    3 to (counts[4]?:0),
+                    4 to (counts[5]?:0),
+                    5 to (counts[6]?:0),
+                    6 to ((counts[0]?:0)+(counts[7]?:0)),
+                    7 to (counts[8]?:0),
+                    8 to (counts[0]?:0)
+            )
+            println("Iteration $it - count ${counts.values.sum()}")
+        }
+        return counts.values.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var counts =
+                input[0].splitToInts().groupBy { it }.entries.map { e -> e.key to e.value.size.toLong() }.toMap()
+
+        (0..255).forEach {
+            counts = mapOf(
+                    0 to (counts[1]?:0),
+                    1 to (counts[2]?:0),
+                    2 to (counts[3]?:0),
+                    3 to (counts[4]?:0),
+                    4 to (counts[5]?:0),
+                    5 to (counts[6]?:0),
+                    6 to ((counts[0]?:0)+(counts[7]?:0)),
+                    7 to (counts[8]?:0),
+                    8 to (counts[0]?:0)
+            )
+            println("Iteration $it - count ${counts.values.sum()}")
+        }
+        return counts.values.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 5)
-    check(part2(testInput) == 12)
+    check(part1(testInput) == 5934)
+    check(part2(testInput) == 26984457539L)
 
 
     val input = readInput("Day06")
--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,8 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun List<String>.asInts(): List<Int> = map{Integer.parseInt(it.trim())}
+
+fun String.splitToInts(sep : Char = ',') = trim().split(sep).filter{it.length>0}.map{Integer.parseInt(it.trim())}
+
