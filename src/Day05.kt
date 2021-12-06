46703bab711601e05694be98e39cf98b91fb433f
Peter Hart <peter.hart@supplydynamics.com>
2021-12-06 10:37:14 -0500

    day 06 files

--- src/Day05.kt
+++ src/Day05.kt
@@ -39,7 +39,7 @@ fun main() {
                 }
             }
         }
-        
+
         return m.filterValues { it > 1 }.size
     }
 
---
+++ src/Day06.kt
@@ -0,0 +1,23 @@
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == 5)
+    check(part2(testInput) == 12)
+
+
+    val input = readInput("Day06")
+    println(part1(input))
+    println(part2(input))
+}
