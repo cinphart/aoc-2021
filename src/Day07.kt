6a78fb4b5d819a1ec045bd4ecb00c86beeec68fb
Peter Hart <cinphart@gmail.com>
2021-12-06 22:09:50 -0500

    Day 07 Initial Files

---
+++ src/Day07.kt
@@ -0,0 +1,23 @@
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 1)
+    check(part2(testInput) == 1)
+
+
+    val input = readInput("Day07")
+    println(part1(input))
+    println(part2(input))
+}
