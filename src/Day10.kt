7b3fb92a2b884311ff07e359031af50fd5d61fe3
Peter Hart <cinphart@gmail.com>
2021-12-10 07:51:35 -0500

    Day 10 part 1 solution.

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,20 +1,59 @@
 fun main() {
 
+    val scores = mapOf(
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137
+    )
+
+    val expect = mapOf(
+        '(' to ')',
+        '[' to ']',
+        '{' to '}',
+        '<' to '>'
+    )
+
+    fun matchPairs(l: String, expected: Char) : Pair<Int, String> {
+        //println("expecting: ${expected}, input: $l")
+        if (l.isEmpty()) return Pair(0, l)
+        var rest = l.drop(1)
+        var lookingAt = l[0]
+        while (lookingAt in expect.keys) {
+            //println("got new start ${lookingAt}")
+            val (score, left) = matchPairs(rest, expect[lookingAt]!!)
+            if (score != 0) return Pair(score, "")
+            if (left.isEmpty()) return Pair(0, "")
+            rest = left.drop(1)
+            lookingAt = left[0]
+        }
+        //println("matching ${lookingAt}:${rest} to ${expected}")
+        if (lookingAt == expected) return Pair(0, rest)
+        return Pair(scores[lookingAt]?:0,"")
+    }
+
+    fun scoreInvalidLine(l: String): Int =
+        matchPairs(l.drop(1), expect[l[0]]!!).first.apply {
+            println("Score: ${l} = ${this}")
+        }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { scoreInvalidLine(it) }.sum()
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
+    check(scoreInvalidLine("([][]>") == 25137)
+
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 15)
+    check(part1(testInput) == 26397)
 
     val input = readInput("Day10")
     println(part1(input))
 
-    check(part2(testInput) == 1134)
+    check(part2(testInput) == 288957)
     println(part2(input))
 }
