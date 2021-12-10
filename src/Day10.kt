28cd99f73626a5edb907c22306e906223b267bf7
Peter Hart <peter.hart@supplydynamics.com>
2021-12-10 10:41:41 -0500

    Day 10 Part 2 Solution

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,10 +1,17 @@
 fun main() {
 
-    val scores = mapOf(
+    val invalidScores = mapOf(
         ')' to 3,
         ']' to 57,
         '}' to 1197,
         '>' to 25137
+    )
+
+    val incompleteScores = mapOf(
+        ')' to 1L,
+        ']' to 2L,
+        '}' to 3L,
+        '>' to 4L
     )
 
     val expect = mapOf(
@@ -14,35 +21,62 @@ fun main() {
         '<' to '>'
     )
 
-    fun matchPairs(l: String, expected: Char) : Pair<Int, String> {
-        //println("expecting: ${expected}, input: $l")
+    fun findInvalidPairs(l: String, expected: Char) : Pair<Int, String> {
         if (l.isEmpty()) return Pair(0, l)
         var rest = l.drop(1)
         var lookingAt = l[0]
         while (lookingAt in expect.keys) {
-            //println("got new start ${lookingAt}")
-            val (score, left) = matchPairs(rest, expect[lookingAt]!!)
+            val (score, left) = findInvalidPairs(rest, expect[lookingAt]!!)
             if (score != 0) return Pair(score, "")
             if (left.isEmpty()) return Pair(0, "")
             rest = left.drop(1)
             lookingAt = left[0]
         }
-        //println("matching ${lookingAt}:${rest} to ${expected}")
         if (lookingAt == expected) return Pair(0, rest)
-        return Pair(scores[lookingAt]?:0,"")
+        return Pair(invalidScores[lookingAt]?:0,"")
     }
 
     fun scoreInvalidLine(l: String): Int =
-        matchPairs(l.drop(1), expect[l[0]]!!).first.apply {
-            println("Score: ${l} = ${this}")
-        }
+        findInvalidPairs(l.drop(1), expect[l[0]]!!).first
 
     fun part1(input: List<String>): Int {
         return input.map { scoreInvalidLine(it) }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun findIncompletePairs(l: String, expected: Char) : Pair<String, String> {
+        if (l.isEmpty()) return Pair("${expected}", "")
+        var rest = l.drop(1)
+        var lookingAt = l[0]
+        while (lookingAt in expect.keys) {
+            val (missing, left) = findIncompletePairs(rest, expect[lookingAt]!!)
+            if (!missing.isEmpty()) return Pair("${missing}${expected}", "")
+            if (left.isEmpty()) return Pair("${expected}", "")
+            rest = left.drop(1)
+            lookingAt = left[0]
+        }
+        if (lookingAt == expected) return Pair("", rest)
+        // Unexpected condition!
+        check("should not" == "get here")
+        return Pair("","")
+    }
+
+
+    fun scoreIncompleteLine(l : String): Long {
+        var left = l
+        var incomplete = ""
+        do {
+            val c = findIncompletePairs(left.drop(1), expect[left[0]]!!)
+            left = c.second
+            incomplete = c.first
+        } while (!left.isEmpty())
+        return incomplete.fold(0L) {
+            a, c -> a*5L + incompleteScores[c]!!
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        val scores = input.filter {scoreInvalidLine(it) == 0 }.map { scoreIncompleteLine(it)}.sorted()
+        return scores[scores.size/2]
     }
 
     check(scoreInvalidLine("([][]>") == 25137)
@@ -54,6 +88,6 @@ fun main() {
     val input = readInput("Day10")
     println(part1(input))
 
-    check(part2(testInput) == 288957)
+    check(part2(testInput) == 288957L)
     println(part2(input))
 }
